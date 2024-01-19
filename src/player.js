f63265fb2868b3025d790b160ba846f3815d5c7a
hartgeld <mail@holgerardelt.de>
2024-01-19 13:53:38 +0100

    bit of styling buttons and stuff ..

--- src/player.js
+++ src/player.js
@@ -1,32 +1,35 @@
-const playBtn = document.querySelector('.play');
-const muteBtn = document.querySelector('.mute');
-const unmuteBtn = document.querySelector('.unmute');
+document.addEventListener('DOMContentLoaded', (event) => {
 
-let id;
+    const playBtn = document.querySelector('.play');
+    const muteBtn = document.querySelector('.mute');
+    const unmuteBtn = document.querySelector('.unmute');
 
-let sound= new Howl({
-    src: [
-        'https://azura.holgerardelt.de/listen/klo_radio_/radio.mp3',
-        'https://azura.holgerardelt.de/listen/klo_radio_/radio.aac',
-        'https://azura.holgerardelt.de/listen/klo_radio_/radio.ogg'
-    ],
-    html5: true, // A live stream can only be played through HTML5 Audio.
-    format: ['mp3', 'aac', 'ogg']
+    let id;
+
+    let sound= new Howl({
+        src: [
+            'https://azura.holgerardelt.de/listen/klo_radio_/radio.mp3',
+            'https://azura.holgerardelt.de/listen/klo_radio_/radio.aac',
+            'https://azura.holgerardelt.de/listen/klo_radio_/radio.ogg'
+        ],
+        html5: true, // A live stream can only be played through HTML5 Audio.
+        format: ['mp3', 'aac', 'ogg']
+    });
+
+    console.log("play");
+
+    playBtn.onclick = function() { id = sound.play(); }
+    muteBtn.onclick = function() { sound.mute(true, id); }
+    unmuteBtn.onclick = function() { sound.mute(false, id); }
+
+    sound.on('end', function(){ playBtn.disabled = false; });
+    sound.on('play', function(){ playBtn.disabled = true; });
+
+    sound.on('loaderror', function(id, error) {
+        console.error('Load error', error);
+    });
+    
+    sound.on('playerror', function(id, error) {
+        console.error('Play error', error);
+    });
 });
-
-console.log("play");
-
-playBtn.onclick = function() { id = sound.play(); }
-muteBtn.onclick = function() { sound.mute(true, id); }
-unmuteBtn.onclick = function() { sound.mute(false, id); }
-
-sound.on('end', function(){ playBtn.disabled = false; });
-sound.on('play', function(){ playBtn.disabled = true; });
-
-sound.on('loaderror', function(id, error) {
-    console.error('Load error', error);
-  });
-  
-  sound.on('playerror', function(id, error) {
-    console.error('Play error', error);
-  });
