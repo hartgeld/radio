3b7a62a42065968d0e48efb5fce4105ff011caee
hartgeld <mail@holgerardelt.de>
2024-02-15 17:22:30 +0100

    offcanvas-nav close after item clicked

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // src/index.js
 import 'uikit/dist/css/uikit.min.css';
 import '@fortawesome/fontawesome-free/js/all';
-// import { Howl } from 'howler';
-// import UIkit from 'uikit';
+import UIkit from 'uikit';
 import { initializePlayer } from './player';
 import { fetch_player_metaInfo } from './player_meta-info.js';
 import { fetchPages } from './fetch-pages.js';
@@ -13,15 +12,29 @@ window.lazySizesConfig.expand = 100; // set to your desired value
 
 import 'lazysizes';
 
+// Function to attach event listeners
+function attachOffcanvasListeners() {
+  document.querySelectorAll('.offcanvas-link').forEach(function(link) {
+    link.addEventListener('click', function() {
+      console.log("close offcanvas-nav");
+      UIkit.offcanvas('#offcanvas-nav').hide();
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {   
   document.body.style.display = 'block'; // Show the body
   initializePlayer();
   fetchPages(); 
-    // Hide the preloader
-    var preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.style.display = 'none';
-    } 
+
+  // Attach event listeners
+  attachOffcanvasListeners();
+
+  // Hide the preloader
+  var preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.style.display = 'none';
+  } 
 });
 
 setInterval(fetch_player_metaInfo, 5000);
