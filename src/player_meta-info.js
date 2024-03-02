fc8297082d3b560289be5a91731d069e7d2da8b5
hartgeld <mail@holgerardelt.de>
2024-03-02 13:09:13 +0100

    streamer name displays in livestream player

--- src/player_meta-info.js
+++ src/player_meta-info.js
@@ -6,14 +6,23 @@ export function fetch_player_metaInfo() {
         const streamerName = data.live.streamer_name;
         var isLiveElement = document.getElementById('isLive');
         var isNotLiveElement = document.getElementById('isNotLive');
+
+        var streamerName_LivestreamStopped = document.getElementById('streamername-LivestreamStopped');
+        var streamername_LivestreamPlaying = document.getElementById('streamername-LivestreamPlaying');
+
         if (isLive) {
             isLiveElement.style.display = 'block';
             isNotLiveElement.style.display = 'none';
             isLiveElement.innerHTML = `<i class="fas fa-circle fa-xs"></i> Live`;
+            streamerName_LivestreamStopped.innerHTML = streamerName;
+            streamername_LivestreamPlaying.innerHTML = streamerName;
+
         } else {
             isLiveElement.style.display = 'none';
             isNotLiveElement.style.display = 'block';
             isNotLiveElement.innerHTML = `<i class="fas fa-repeat fa-xs"></i> Replay`;
+            streamerName_LivestreamStopped.innerHTML = streamerName;
+            streamername_LivestreamPlaying.innerHTML = streamerName;
         }
         return {
             isLive: isLive,
